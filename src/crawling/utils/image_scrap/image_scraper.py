0f75908ff1d248bb10ddf5232e40bc18c8fbf105
JminJ <jminju254@gmail.com>
2024-09-08 23:24:36 +0900

    UPDATE: modify code for clean

--- src/crawling/utils/image_scrap/image_scraper.py
+++ src/crawling/utils/image_scrap/image_scraper.py
@@ -4,7 +4,7 @@ from playwright.async_api import async_playwright, Playwright
 
 async def take_screenshot(url: str) -> bytes:
     playwright = await async_playwright().start()
-    browser = await playwright.chromium.launch(headless=False)
+    browser = await playwright.chromium.launch(headless=True)
     try:
         page = await browser.new_page()
         await page.goto(url)
@@ -18,37 +18,3 @@ async def take_screenshot(url: str) -> bytes:
     finally:
         await browser.close()
         await playwright.stop()
-        
-# async def take_screenshot(url: str) -> bytes:
-#     playwright_cli = await async_playwright().start()
-#     browser = await playwright_cli.chromium.launch()
-#     try:
-#         # 웹페이지 열기
-#         page = await browser.new_page()
-#         await page.goto(url)
-
-#         page = await page.wait_for_selector("//body", timeout=30000)
-#         screenshot = await page.screenshot()
-
-#         return screenshot
-#     except Exception as E:
-#         print(traceback.format_exc())
-#         print(E)
-#         raise E
-#     finally:
-#         await browser.close()
-#         await playwright_cli.stop()
-
-# if __name__ == "__main__":
-#     screenshots = take_screenshot(
-#         "https://n.news.naver.com/article/584/0000028235?cds=news_media_pc&type=editn"
-#     )
-#     print(screenshots)
-
-async def main():
-    screenshot = await take_screenshot(url="https://n.news.naver.com/article/584/0000028235?cds=news_media_pc&type=editn")
-    print(screenshot)
-
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
